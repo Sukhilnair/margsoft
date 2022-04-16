bde2493347d99c49824f3e9f07ecb38821c6d998
Sukhilnair <102207536+Sukhilnair@users.noreply.github.com>
2022-04-16 09:43:44 +0530

    Update anpr_update.sh

--- anpr_update.sh
+++ anpr_update.sh
@@ -6,10 +6,12 @@ count=`cat /tmp/list.txt | wc -w`
 i=1
 while [ $i -le $count ]
 do
-    cut -d: -f $i /tmp/list.txt | grep CAM
-    echo $i
+    cut -d: -f $i /tmp/list.txt | grep CAM >> /tmp/list2.txt
+    #echo $i
     i=$(($i+1))
 done
+echo "##############################################################################################################################"
+echo "Cameras Which are active:"
 cat /tmp/list2.txt
 rm /tmp/list2.txt
-    
+echo "##############################################################################################################################"
