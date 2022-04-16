0254954a64c619a70de9b225f70fc91c8cacd19a
Sukhilnair <102207536+Sukhilnair@users.noreply.github.com>
2022-04-16 10:10:01 +0530

    Update anpr_update.sh

--- anpr_update.sh
+++ anpr_update.sh
@@ -17,15 +17,16 @@ rm /tmp/list2.txt
 echo "##############################################################################################################################"
 echo "Do you want to activate/deactivate, type anything else for exit"
 read todo
-    echo "Please mention which instance: "
-    read inst
     if [ "$todo" == "activate" ]
     then
-        curl -X PUT "http://0.0.0.0:5000/uvanpr/v2/cameras/$inst" -d '{ "activated": true }' -H "Content-Type: application/json"
+            echo "Please mention which instance: "
+            read inst
+            curl -X PUT "http://0.0.0.0:5000/uvanpr/v2/cameras/$inst" -d '{ "activated": true }' -H "Content-Type: application/json"
     elif ["$todo" == "deactivate" ]
     then
-        curl -X PUT "http://0.0.0.0:5000/uvanpr/v2/cameras/$inst" -d '{ "activated": false }' -H "Content-Type: application/json"
+            echo "Please mention which instance: "
+            read inst
+            curl -X PUT "http://0.0.0.0:5000/uvanpr/v2/cameras/$inst" -d '{ "activated": false }' -H "Content-Type: application/json"
     else
-        break;
     fi
         
